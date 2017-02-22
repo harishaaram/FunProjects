318e36e94469cd4dc2b230e53801d4972b351b85
harish gandhi ramachandran <hramachandran.haw.iit.edu>
2017-02-21 18:21:42 -0600

    same filename with diff path is also saved

--- open_websites.py
+++ open_websites.py
@@ -9,6 +9,7 @@ html = urllib.urlopen(url).read()
 soup = BeautifulSoup(html)
 soup.prettify()#html document with proper indentation
 
+#initialization
 tags = soup.findAll('a', href=True)
 count = 0
 saveas = []
@@ -16,13 +17,26 @@ already_exist = bool
 
 
 #Checking in the local folder for the filename
-def check_for_file(fname, path):
-        for root, dirs, files in os.walk(path):
+def check_for_file(fname):
+        for root, dirs, files in os.walk(dir_path):
             if fname in files:
                 return True
         return False
 
+#saving with the word after the last two(because two pdf with same name can exist in different path) backward slash
+def write_to_folder(saveas_splited):
+    if len(saveas_splited) > 1:
+        save_filename = saveas_splited[len(saveas_splited) -2] +'_'+saveas_splited[len(saveas_splited) -1]
+        already_exist = check_for_file(save_filename)
+        if already_exist == False:
+            urllib.urlretrieve(download_link,dir_path+save_filename)
+    else:
+        already_exist = check_for_file(saveas_splited[0])
+        if already_exist == False:
+            urllib.urlretrieve(download_link,dir_path+saveas_splited[0])
+    return True
 
+#looping through links
 for link in tags:
     filename = link.get('href')
     download_link = url + filename
@@ -30,13 +44,6 @@ for link in tags:
     if filename[len(filename)-3:] == 'pdf':
         saveas = filename
         saveas_splited = saveas.split('/')
+        write_to_folder(saveas_splited)
 
-        #saving with the word after the last backward slash
-        if len(saveas_splited) > 1:
-            already_exist = check_for_file(saveas_splited[len(saveas_splited) -1],dir_path)
-            if already_exist == False:
-                urllib.urlretrieve(download_link,dir_path+saveas_splited[len(saveas_splited) -1])
-        else:
-            already_exist = check_for_file(saveas_splited[0],dir_path)
-            if already_exist == False:
-                urllib.urlretrieve(download_link,dir_path+saveas_splited[0])
+
